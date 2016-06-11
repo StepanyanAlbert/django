5727de6ef920897d094faa3a1aac050601fb8d2e
Stepanyan Albert <1felisidad1@mail.ru>
2016-06-11 13:46:46 +0400

    Navbar added MCloud

--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404
-from .models import Album
+from .models import Album,Song
 
 def index(request):
     all_albums=Album.objects.all()
@@ -8,8 +8,24 @@ def index(request):
 
 
 def detail(request,album_id):
-    try:
-       album=Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-       raise Http404('Album does not exists!!!') 
+    #try:
+    #   album=Album.objects.get(pk=album_id)
+    #except Album.DoesNotExist:
+    #   raise Http404('Album does not exists!!!') 
+    album=get_object_or_404(Album,pk=album_id)
     return render(request,'music/detail.html',{'album':album})
+
+def favorite(request,album_id):
+    album=get_object_or_404(Album,pk=album_id)
+    try:
+       sel_song=album.song_set.get(pk=request.POST['song'])
+    except (KeyError,Song.DoesNotExist):
+       return render(request,'music/detail.html',{'album':album, 'error_message':'Not a valid song'}) 
+    else: 
+       if sel_song.is_favorite:
+          sel_song.is_favorite=False
+          sel_song.save()
+       else:
+          sel_song.is_favorite=True
+          sel_song.save()  
+       return render(request,'music/detail.html',{'album':album} )
